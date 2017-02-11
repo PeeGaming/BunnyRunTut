0e804e3bef91e8e4fd567d050888003403d9396a
PeeGaming <peeyushchaturvedius@gmail.com>
2017-02-11 14:35:28 +0530

    Added Bunny Hurt and Score.

--- Assets/MoveLeft.cs
+++ Assets/MoveLeft.cs
@@ -13,6 +13,6 @@ public class MoveLeft : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position += Vector3.left * speed * Time.deltaTime;
+        transform.position += Vector3.left * speed * Time.deltaTime; 
 	}
 }
